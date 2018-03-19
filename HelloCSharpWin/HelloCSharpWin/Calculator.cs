480e3b43718bbdc216d23a54e74f76c212f67d6e
ch9294 <ch9294@naver.com>
2018-03-19 20:39:00 +0900

    18-03-19 1차 수정

--- HelloCSharpWin/HelloCSharpWin/Calculator.cs
+++ HelloCSharpWin/HelloCSharpWin/Calculator.cs
@@ -42,13 +42,60 @@ namespace HelloCSharpWin
 
         private void SumNumbers_Click(object sender, EventArgs e)
         {
-            SumResult.Text = Add(Convert.ToInt32(Sum1.Text),Convert.ToInt32(Sum2.Text)).ToString();
-        }
+            int num1 = 0;
+            int num2 = 0;
 
+            if(string.IsNullOrWhiteSpace(Sum1.Text))
+            {
+                MessageBox.Show("숫자를 입력해주세요");
+                Sum1.Focus();
+                return;
+            }
+
+          
+           if (int.TryParse(Sum1.Text,out num1) == false)
+            {
+                MessageBox.Show("문자가 들어왔습니다.");
+                Sum1.SelectAll();
+                
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(Sum2.Text))
+            {
+                MessageBox.Show("숫자를 입력해주세요");
+                return;
+            }
+
+            if(int.TryParse(Sum2.Text,out num2))
+            {
+                MessageBox.Show("문자가 들어왔습니다.");
+                return;
+            }
+            num1 = Convert.ToInt32(Sum1.Text);
+            num2 = Convert.ToInt32(Sum2.Text);
+            SumResult.Text = Add(num1,num2).ToString();
+        }
+        
+        // 메소드 오버로딩에 영향을 미치는 것은 매개변수의 갯수와 타입이다.
+        // 반환 형식은 오버로딩의 조건에 부합하지 않는다.
         int Add(int num1,int num2)
         {
+            
             int sum = num1 + num2;
             return sum;
+        }
+
+        public float Add(float num1, float num2)
+        {
+            float sum = num1 + num2;
+            return sum;
+        }
+
+        public int Sub(int num1,int num2)
+        {
+            int sub = num1 - num2;
+            return sub;
         }
     }
 }
